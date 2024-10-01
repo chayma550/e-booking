824f4e91563f8f962a782cc094ea8c76f200f7ba
chayma550 <rachayma@gmail.com>
2024-10-01 17:11:51 +0200

    Update CORS

--- api/index.js
+++ api/index.js
@@ -3,47 +3,38 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import hotelsRoute from "./routers/hotel.js";
 import authRoute from "./routers/auth.js";
-import userRoute from "./routers/users.js"
-import roomRoute from "./routers/rooms.js"
-import cors from "cors"
+import userRoute from "./routers/users.js";
+import roomRoute from "./routers/rooms.js";
+import cors from "cors";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 
-const app=express();
+const app = express();
 dotenv.config();
 
-//connect to mongodb
-mongoose.connect(process.env.MONGO_URL).then(()=>
-console.log("DB connecting ")).catch((err)=>{
-  console.log(err)  
-})
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("DB connected"))
+  .catch(err => console.log(err));
 
-
-
-//middlewares:
-// Configure CORS
+// Middleware
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || "http://localhost:3000", // Allow requests from your frontend
-  credentials: true,
+  origin: process.env.FRONTEND_URL || "http://localhost:3000", // Set this to your frontend's URL
+  credentials: true, // Allow credentials to be sent
 };
-app.use(cors(corsOptions)); // This line was missing
-app.use(bodyParser.json());
 
+// Use CORS before defining routes
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
 app.use(cookieParser());
 
-//routers:
-app.use("/api/auth",authRoute)
-app.use("/api/hotels",hotelsRoute)
-app.use("/api/users",userRoute)
-app.use("/api/rooms",roomRoute)
+// Routes
+app.use("/api/auth", authRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/users", userRoute);
+app.use("/api/rooms", roomRoute);
 
-
-
-
-
-
-
-//connect to server
-app.listen(process.env.PORT||8000,()=>{
+// Connect to server
+app.listen(process.env.PORT || 8000, () => {
   console.log("Server running on port " + (process.env.PORT || 8000));
-})
+});
